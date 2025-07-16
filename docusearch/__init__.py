7f09f726c8544412901eb6ddc15ad21a59becee9
Moye <moyeodiase@hotmail.co.uk>
2025-07-16 19:19:00 +0100

    Remove py files

--- docusearch/__init__.py
+++
@@ -1,9 +0,0 @@
-from docusearch.cli import PROJECT_DESCRIPTION
-
-from .index import ForwardIndex, ReverseIndex
-from .storage import DocumentStorage
-from .trie import Trie
-
-__version__ = "0.1.0"
-__all__ = ["DocumentStorage", "Trie", "ForwardIndex", "ReverseIndex"]
-__doc__ = PROJECT_DESCRIPTION
